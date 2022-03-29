dba84674e49d0452c9e44b4806e99cdb5330dbbb
Ongezwa <ongezwaf27@gmail.com>
2022-03-29 15:35:46 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// console.log(exios);
+let carsElement = document.querySelector(".cars")
+axios
+.get("https://api-tutor.herokuapp.com/v1/cars")
+.then(function(display){
+    console.log(display.data);
+    display.data.forEach(cars => {
+        let li = document.createElement("tr")
+        li.innerHTML = `<tr> 
+        <td>${cars.model}<td/>
+        <td>${cars.make}<td/>
+        <td>${cars.color}<td/> `
+        
+
+        
+        carsElement.appendChild(li)
+    
+    });
+   
+
+    
+})
