b6943afd6184f69cd4db2dda5e355ef0b09b3911
Ongezwa <ongezwaf27@gmail.com>
2022-04-01 16:22:01 +0200

    filtering

--- index.js
+++ index.js
@@ -1,72 +1,87 @@
 // console.log(exios);
-let carsElement = document.querySelector(".cars")
+let carsElement = document.querySelector(".cars");
+let colorsElement = document.querySelector(".colours");
+let colorsElem = document.querySelector(".col");
+let makeElement = document.querySelector(".make");
+let brandsElement = document.querySelector(".brands");
+
+
 axios
-.get("https://api-tutor.herokuapp.com/v1/cars")
-.then(function(display){
-    console.log(display.data);
-    display.data.forEach(cars => {
-        let li = document.createElement("tr")
-        li.innerHTML = `<tr> 
-        <td>${cars.model}</td>
-        <td>${cars.make}</td>
-        <td>${cars.color}</td> 
-        </tr>`
-        
-        carsElement.appendChild(li)
-    
-    });
-   
+    .get("https://api-tutor.herokuapp.com/v1/cars")
+    .then(function (display) {
+        console.log(display.data);
+        display.data.forEach(cars => {
+            let li = document.createElement("tr")
 
-    
-})
-
-// // const carsElement = document.querySelector(".cars");
-// const colors = document.querySelector(".colors");
-// const enterElem = document.querySelector(".enter");
-// const displayElem = document.querySelector(".display");
-
-// let carList = [{ make: 'Volkswagen', model: 'Touran', color: 'Red' },
-// { make: 'Volkswagen', model: 'Jetta', color: 'Blue' },
-// { make: 'Ford', model: 'Ranger', color: 'White' },
-// { make: 'Nissan', model: 'Qashqai', color: 'Orange' },
-// { make: 'Toyota', model: 'Corolla', color: 'Orange' },
-// { make: 'Nissan', model: 'Micra', color: 'Blue' },
-// { make: 'Nissan', model: 'Sentra', color: 'Orange' },
-// { make: 'Volkswagen', model: 'Jetta', color: 'Orange' },
-// { make: 'Ford', model: 'Fiesta', color: 'Blue' },
-// { make: 'Nissan', model: 'Juke', color: 'Blue' },
-// { make: 'Ford', model: 'Focus', color: 'Orange' },
-// { make: 'Ford', model: 'EcoSport', color: 'Red' },
-// { make: 'Nissan', model: 'Qashqai', color: 'Blue' },
-// { make: 'Toyota', model: 'Yaris', color: 'Orange' },
-// { make: 'Toyota', model: 'Yaris', color: 'Blue' },
-// { make: 'Toyota', model: 'Corolla', color: 'Blue' },
-// { make: 'Nissan', model: 'Micra', color: 'Blue' },
-// { make: 'Ford', model: 'Fiesta', color: 'Orange' },
-// { make: 'Nissan', model: 'Micra', color: 'White' },
-// { make: 'Nissan', model: 'Qashqai', color: 'Red' },
-// { make: 'Nissan', model: 'Sentra', color: 'Red' }];
+                li.innerHTML = `<tr> 
+            <td>${cars.model}</td>
+            <td>${cars.make}</td>
+            <td>${cars.color}</td> 
+            <td>${cars.price}</td> 
+            <td>${cars.reg_number}</td> 
 
 
-// let displayList = async () => {
-//     // alert("google")
-//     let list = await axios.get("https://api-tutor.herokuapp.com/v1/cars")
-//         .then(function (display) {
-//             console.log(display.data);
-//             display.data.forEach(cars => {
-//                 let li = document.createElement("tr")
-//                 li.innerHTML = `<tr> 
-//         <td>${cars.color}<td/> `
+            </tr>`
 
-//                 carsElement.appendChild(li)
-//                 console.log(cars);
+            carsElement.appendChild(li)
 
-//             });
+        });
 
 
 
-//         })
- 
-        
-// }
-// enterElem.addEventListener("click",displayList)
+    })
+
+
+axios.get("https://api-tutor.herokuapp.com/v1/colors")
+    .then(function (results) {
+        // console.log(results.data)
+        results.data.forEach(color => {
+            let li = document.createElement("li")
+            li.innerHTML = `<li>${color}</li>`
+            colorsElement.appendChild(li)
+            console.log(li);
+        })
+    })
+
+
+axios.get("https://api-tutor.herokuapp.com/v1/makes")
+    .then(function (params) {
+        // console.log(params.data);
+        params.data.forEach(makes => {
+            let list = document.createElement("list")
+            list.innerHTML = `<li>${makes}</li>`
+            makeElement.appendChild(list)
+
+        })
+
+    })
+
+// const carsElement = document.querySelector(".cars");
+const colors = document.querySelector(".colors");
+const enterElem = document.querySelector(".enter");
+const displayElem = document.querySelector(".display");
+
+
+
+
+let displayList = () => {
+
+    axios.get(`https://api-tutor.herokuapp.com/v1/cars/make/${brandsElement.value}/color/${colorsElem.value}`)
+        .then(function (display) {
+            console.log(display.data[0]);
+            display.data.forEach(cars => {
+
+            
+
+                    let li = document.createElement("tr")
+                    li.innerHTML = `<tr> 
+                    <td>${cars.model}</td>
+                <td>${cars.make}</td>
+                <td>${cars.color}</td>`
+                
+                carsElement.appendChild(li)
+            });
+        })
+}
+
+enterElem.addEventListener("click", displayList)
